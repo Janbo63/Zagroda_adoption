678184c2f4d02034eff1a4c3603c251bc38cb247
Janbo63 <jan@futuresolutionsjf.com>
2024-11-05 19:35:00 +0000

    babel

--- next.config.js
+++ next.config.js
@@ -8,21 +8,13 @@ const nextConfig = {
     unoptimized: true,
     remotePatterns: [],
   },
-  experimental: {
-    optimizePackageImports: ['@radix-ui/react-icons', 'lucide-react']
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  staticPageGenerationTimeout: 120,
-  output: 'standalone',
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
+  output: 'standalone'
 };
 
 module.exports = withNextIntl(nextConfig);
