919e900e3ebe30adcefd54194e0cc01fdb7d6300
Janbo63 <jan@futuresolutionsjf.com>
2024-11-05 21:36:43 +0000

    home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,14 @@
-// Static - Homepage can remain static
-export default function HomePage() {
-  // ... your code
+import {unstable_setRequestLocale} from 'next-intl/server';
+import { HomeContent } from '@/components/HomeContent';
+
+type Props = {
+  params: {locale: string}
+};
+
+export default function HomePage({params: {locale}}: Props) {
+  unstable_setRequestLocale(locale);
+  
+  return (
+    <HomeContent locale={locale} />
+  );
 }
