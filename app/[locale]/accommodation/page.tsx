aed58432cab0d2fba6d00f2ecf064413963f6f98
Janbo63 <jan@futuresolutionsjf.com>
2024-11-06 18:58:42 +0000

    id

--- app/[locale]/accommodation/page.tsx
+++ app/[locale]/accommodation/page.tsx
@@ -11,33 +11,12 @@ export default function AccommodationPage({ params: { locale } }: AccommodationP
     <div className="flex flex-col min-h-screen">
       <div className="container mx-auto px-4 py-8 mt-16">
         <iframe 
-          data-src="https://beds24.com/booking2.php?propid=YOUR_PROPERTY_ID&referer=iFrame" 
+          src="https://beds24.com/booking2.php?propid=	98031&referer=iFrame" 
           width="100%" 
           height="800" 
           style={{ border: 'none', maxWidth: '100%', overflow: 'auto' }}
+          title="Booking System"
         />
-        <script dangerouslySetInnerHTML={{ __html: `
-          var addUrlParamsToIframeSrcs = function() {
-            const validParameters = ["checkin", "checkout", "numnight", "numadult", "numchild", "ownerid", "propid", "roomid", "referer", "lang", "group", "nogroup", "category1", "category2", "category3", "category4", "customParameter"];
-            const currentUrl = new URL(window.location.href);
-            let parametersString = "";
-            validParameters.forEach(parameter => {
-              const parameterValue = currentUrl.searchParams.get(parameter);
-              if (parameterValue !== null) {
-                parametersString += "&" + parameter + "=" + parameterValue;
-              }
-            });
-            const iframes = document.getElementsByTagName("iframe");
-            for (let iframe of iframes) {
-              let iframeSrc = iframe.getAttribute("data-src");
-              if (iframeSrc === null) continue;
-              if (!iframeSrc.includes("?")) iframeSrc += "?";
-              iframeSrc += parametersString;
-              iframe.setAttribute("src", iframeSrc)
-            }
-          };
-          addUrlParamsToIframeSrcs();
-        `}} />
       </div>
     </div>
   )
