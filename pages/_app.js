b98b579da439a1dfe405414b9f71cc80bf970a1e
Janbo63 <jan@futuresolutionsjf.com>
2024-12-15 12:13:51 +0100

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      gtag.pageview(url)
+      window.gtag('config', 'G-V9R1JJYYSG', {
+        page_path: url,
+      })
     }
+
     router.events.on('routeChangeComplete', handleRouteChange)
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange)
--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { GA_MEASUREMENT_ID } from '../lib/gtag'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-V9R1JJYYSG`}
+          strategy="afterInteractive"
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_MEASUREMENT_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V9R1JJYYSG');
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
