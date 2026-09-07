712f48ab591c936f9622e16850447a21171e8270
Janbo63 <jan@futuresolutionsjf.com>
2026-09-07 11:24:28 +0200

    feat(analytics): add primary ad account Meta Pixel 1054665503839276

--- components/FacebookPixel.tsx
+++ components/FacebookPixel.tsx
@@ -31,6 +31,7 @@ const FacebookPixel = () => {
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '${fpixel.FB_PIXEL_ID}');
+            fbq('init', '1054665503839276');
             fbq('track', 'PageView');
           `,
                 }}
